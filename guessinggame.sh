5e2128f90644051b0062cd93fc8e732a74ffe12e
younes boujrada <113713385+younes-boujrada@users.noreply.github.com>
2025-01-17 15:06:09 +0100

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,13 +1,21 @@
-*.tar
-*.tar.*
-*.jar
-*.exe
-*.msi
-*.zip
-*.tgz
-*.log
-*.log.*
-*.sig
+#!/bin/bash
 
-pkg/
-src/
+# Function to count files and handle guessing logic
+function guess_files {
+    local file_count=$(ls -1 | wc -l)
+    while true; do
+        echo "Guess how many files are in the current directory:"
+        read guess
+        if [[ $guess -eq $file_count ]]; then
+            echo "Congratulations! You guessed the correct number of files: $file_count."
+            break
+        elif [[ $guess -lt $file_count ]]; then
+            echo "Too low. Try again!"
+        else
+            echo "Too high. Try again!"
+        fi
+    done
+}
+
+# Run the guessing function
+guess_files
